a49b45d22418c331b526dab485876c4753d45eca
Tiago Ramos <tiagoaramos@gmail.com>
2025-09-16 20:02:30 -0300

    Docker Hierarquico

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Extend Jest matchers with jest-dom matchers
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeInTheDocument(): R;
+      toHaveTextContent(text: string | RegExp): R;
+      toHaveClass(className: string): R;
+      toHaveAttribute(attr: string, value?: string): R;
+    }
+  }
+}
